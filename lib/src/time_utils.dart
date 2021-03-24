745d8f425d7ad49f690f0e95860c5df2c01111c9
Mohammad Amir Mohammadi <matinmohammadi@MacBook-Pro-som-tillhor-Matin.local>
2021-03-25 01:21:52 +0430

    refactor code

--- lib/src/time_utils.dart
+++ lib/src/time_utils.dart
@@ -1,10 +1,3 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// @dart = 2.8
-
-import 'dart:ui' show hashValues;
 
 import 'package:flutter/material.dart';
 
