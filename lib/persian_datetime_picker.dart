9fe0e63dd8d1227c9f4e39725bcc1cd4a9a8891a
Mohammad Amir Mohammadi <matinmohammadi@MacBook-Pro-som-tillhor-Matin.local>
2021-03-25 01:12:42 +0430

    refactor code

--- lib/persian_datetime_picker.dart
+++ lib/persian_datetime_picker.dart
@@ -1,14 +1,13 @@
 library datetimepicker;
 
-
-export 'pdate_utils.dart';
-export 'ptime_picker.dart';
-export 'pinput_date_picker.dart';
-export 'pcalendar_date_picker.dart';
-export 'pdate_picker_dialog.dart';
-export 'pinput_date_picker.dart';
-export 'pdate_picker_header.dart';
-export 'pdate_picker_deprecated.dart';
-export 'pdate_picker_common.dart';
-export 'time_utils.dart';
+export 'src/ptime_picker.dart';
+export 'src/pdate_utils.dart';
+export 'src/pinput_date_picker.dart';
+export 'src/pcalendar_date_picker.dart';
+export 'src/pdate_picker_dialog.dart';
+export 'src/pinput_date_picker.dart';
+export 'src/pdate_picker_header.dart';
+export 'src/pdate_picker_deprecated.dart';
+export 'src/pdate_picker_common.dart';
+export 'src/time_utils.dart';
 export 'package:shamsi_date/shamsi_date.dart';
