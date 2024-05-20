e7150d6ef523ded64c8a1fbac0dd8ad3194c60eb
s.masoumbeigi <sbeygi712@gmail.com>
2024-05-20 13:49:03 +0330

    removing duplicate export

--- lib/persian_datetime_picker.dart
+++ lib/persian_datetime_picker.dart
@@ -5,7 +5,6 @@ export 'src/pdate_utils.dart';
 export 'src/pinput_date_picker.dart';
 export 'src/pcalendar_date_picker.dart';
 export 'src/pdate_picker_dialog.dart';
-export 'src/pinput_date_picker.dart';
 export 'src/pdate_picker_header.dart';
 export 'src/pdate_picker_deprecated.dart';
 export 'src/pdate_picker_common.dart';
